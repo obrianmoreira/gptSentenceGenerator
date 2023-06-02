6cdf84451a64af2ed03eb268e75b2aec14086fae
obrianmoreira <brian.agnilo@gmail.com>
2023-06-02 12:16:56 +0100

    new adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script id="8956023736" data-ad-client="pub-8886377477645608" async="true" onError={(e) => console.log('error', e)} strategy="beforeInteractive" beforeInteractive src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8886377477645608'/>
       <body>
         <Main />
         <NextScript />
